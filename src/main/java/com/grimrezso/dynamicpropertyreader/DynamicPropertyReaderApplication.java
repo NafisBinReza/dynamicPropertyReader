eb08480708a6e9c7dfd5a4cd2297a96920cf92cf
NafisBinReza <nafisbinreza@gmail.com>
2023-07-17 14:31:03 +0600

    initial commit.

---
+++ src/main/java/com/grimrezso/dynamicpropertyreader/DynamicPropertyReaderApplication.java
@@ -0,0 +1,13 @@
+package com.grimrezso.dynamicpropertyreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicPropertyReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicPropertyReaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grimrezso/dynamicpropertyreader/DynamicPropertyReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grimrezso.dynamicpropertyreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicPropertyReaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
